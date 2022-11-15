99ec2030e160d46fab37ed4e9ef6b67e3314c324
Daniel Zduniak <dzduniak@gmail.com>
2022-11-15 18:37:35 +0100

    Remove worker.js and favicon.svg.

--- www/worker.js
+++
@@ -1,8 +0,0 @@
-importScripts("worker_wasm.js");
-
-async function initWorker() {
-    await wasm_bindgen("worker_wasm_bg.wasm");
-};
-
-console.log("Worker: initializing worker...");
-initWorker();
